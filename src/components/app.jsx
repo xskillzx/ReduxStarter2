f620e33858f6338bc435df224dc4b59d1f84ce70
Moises Munoz <moises_a_munoz@yahoo.com>
2018-05-04 18:37:28 -0700

    add BookTail to App and add a pages property to each book, also adds react-redux plugin to eslint

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <BookList />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <BookList />
+      <BookDetail />
+    </div>
+  );
 }
